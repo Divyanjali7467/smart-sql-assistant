52f60bf30b2aac5422bf9ed436d735727adce910
Divyanjali <2k23.csds2313690@gmail.com>
2026-08-03 00:34:17 +0530

    Update groq_helper.py

--- groq_helper.py
+++ groq_helper.py
@@ -1,6 +1,20 @@
 import streamlit as st
 from groq import Groq
 
-client = Groq(
-    api_key=st.secrets["GROQ_API_KEY"]
-)
+api_key = st.secrets["GROQ_API_KEY"]
+
+client = Groq(api_key=api_key)
+
+
+def ask_llm(prompt):
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    return response.choices[0].message.content
