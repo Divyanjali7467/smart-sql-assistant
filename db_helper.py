26aece5e0b2e68996cec2871edb3229541a9f4d0
Divyanjali <2k23.csds2313690@gmail.com>
2026-08-02 23:57:27 +0530

    Update db_helper.py

--- db_helper.py
+++ db_helper.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 def run_query(query):
-    conn = sqlite3.connect("employee.db")
+    conn = sqlite3.connect("company.db")
     result = pd.read_sql_query(query, conn)
     conn.close()
     return result
