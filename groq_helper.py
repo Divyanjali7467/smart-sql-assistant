d372462842d1f499df900c354d33d419e39c7891
Divyanjali <2k23.csds2313690@gmail.com>
2026-08-03 00:28:21 +0530

    Update groq_helper.py

--- groq_helper.py
+++ groq_helper.py
@@ -1,11 +1,6 @@
 import streamlit as st
 from groq import Groq
 
-api_key = "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"
-client = Groq(api_key=api_key)
-
-if api_key is None:
-    st.error("Groq API key not found.")
-    st.stop()
-
-client = Groq(api_key=api_key)
+client = Groq(
+    api_key=st.secrets["GROQ_API_KEY"]
+)
