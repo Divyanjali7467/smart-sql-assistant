c97016140c2b4aba8bcd14873e34814b3dd1eaf3
Divyanjali <2k23.csds2313690@gmail.com>
2026-08-03 00:20:28 +0530

    Update groq_helper.py

--- groq_helper.py
+++ groq_helper.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from groq import Groq
 
-api_key = "gsk_xxxxxxxxxxxxxxxxx"
+api_key = "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"
 client = Groq(api_key=api_key)
 
 if api_key is None:
